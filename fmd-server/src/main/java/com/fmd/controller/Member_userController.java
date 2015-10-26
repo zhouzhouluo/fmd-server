fffcb5654c13f8f71c661271623028847a8597c0
zhouzhouluo <luoyz619@163.com>
2015-10-26 16:19:27 +0800

    页面

--- fmd-server/src/main/java/com/fmd/controller/Member_userController.java
+++ fmd-server/src/main/java/com/fmd/controller/Member_userController.java
@@ -1,11 +1,14 @@
 package com.fmd.controller;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fmd.entity.Member_user;
 import com.fmd.entity.User;
 import com.fmd.service.Member_userService;
 import com.fmd.service.UserService;
@@ -14,6 +17,7 @@ import com.fmd.service.UserService;
  * @author Administrator
  *
  */
+@RequestMapping("/member")
 @Controller
 public class Member_userController {
 	
@@ -21,8 +25,14 @@ public class Member_userController {
     private Member_userService member_userService;
 	
 	
-	@RequestMapping("/member/save")
-	public String save(){
+	@RequestMapping("/save")
+	public String save(Member_user member_user){
+		member_user.setCjsj(new Date());
+		member_user.setCapital("960");
+		member_user.setPwd1("111111");
+		member_user.setPwd2("222222");
+		member_user.setPwd3("333333");
+		member_userService.save(member_user);
 		return null;
 	}
 	@RequestMapping("/member/update")
